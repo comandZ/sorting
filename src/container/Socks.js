4bc2a2ffe39b3642a7d0e4cf4df2ff306c0bdd07
pachippe1 <patrick.schippers@sofwerx.org>
2022-10-20 09:42:55 -0400

    Updating Repo before Exit

---
+++ src/container/Socks.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Socks = () => {
+
+    const socks = 'white pink white white red pink';
+    const pairs = '';
+
+    console.log('Socks', socks);
+    console.log('Pairs', pairs);
+
+    return;
+}
+
+export default Socks;
